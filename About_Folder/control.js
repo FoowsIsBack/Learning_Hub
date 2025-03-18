7f28efafdb19c9a1282014f26afd21ed7d6660fb
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-03-18 23:15:30 +0800

    Add files via upload

---
+++ About_Folder/control.js
@@ -0,0 +1,19 @@
+function maincontent(page) {
+    let contentDiv = document.getElementById("content");
+
+    if (page === "home") {
+        window.location.href = '../page_content.html';
+    } else if (page === "about") {
+        window.location.href = 'about.html';
+    } else if (page === "c_lang") {
+        window.location.href = '../C_Folder/C_language.html';
+    } else if (page === "python") {
+        window.location.href = '../Python_Folder/Python_language.html';
+    } else if (page === "html") {
+        window.location.href = '../HTML_folder/Html.html';
+    } else if (page === "js") {
+        window.location.href = '../JavaScript_Folder/JavaScript.html';
+    } else if (page === "php") {
+        window.location.href = '../PHP_Folder/PHP.html';
+    }
+}
